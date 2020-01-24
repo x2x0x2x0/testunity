645625e44ba55b0279c23e50a6235fc9d6d28170
Oleksandr Halanin <alexsanify@gmail.com>
2020-01-24 23:12:03 +0200

    Initial commit

---
+++ Assets/Editor/MapGenerator.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(ObjectBuilderScript))]
+public class MapGenerator : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        ObjectBuilderScript myScript = (ObjectBuilderScript)target;
+        if (GUILayout.Button("Generate Map"))
+        {
+            myScript.GenerateMap();
+        }
+        if (GUILayout.Button("Clear Map"))
+        {
+            myScript.ClearMap();
+        }
+    }
+}
+
+
---
+++ Assets/Scripts/ObjectBuilderScript.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class ObjectBuilderScript : MonoBehaviour
+{
+    [SerializeField] private GameObject tile;
+    [SerializeField] private Transform parent;
+    private int upperBound = 10;
+    private int lowestBound = 0;
+
+
+    public void GenerateMap()
+    {
+        for (int x = lowestBound; x <= upperBound; x++)
+        {
+            for (int z = lowestBound; z <= upperBound; z++)
+            {
+                var tilePosition = new Vector3(x, 0, z);
+                GameObject.Instantiate(tile, tilePosition, Quaternion.identity, parent);
+            }
+        }
+    }
+
+    public void ClearMap()
+    {
+        var cubesToDestroy = new List<Transform>();
+        foreach(Transform cube in parent.transform)
+        {
+            cubesToDestroy.Add(cube);
+        }
+
+        cubesToDestroy.ForEach(c => DestroyImmediate(c.gameObject));
+    }
+}
